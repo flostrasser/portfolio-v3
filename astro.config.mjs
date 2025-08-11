dee683775602eb1640de0c256f36df12c30d6a0c
Florian Strasser <56030947+flostrasser@users.noreply.github.com>
2025-08-11 11:34:53 +0200

    Remove astro tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import netlify from '@astrojs/netlify';
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   adapter: netlify(), // for dynamic robots.txt
 });
